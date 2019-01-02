f86eb2a7fcb33bede5e03ad1c57fec5d63994908
ASAPChiul <45797230+ASAPChiul@users.noreply.github.com>
2019-01-02 00:06:57 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+{
+  "botToken": "NTI5OTMyNDQ4NDkxMTEwNDAw.Dw4Btg.GMXQWw27nlRUaR05yWbvy6Tu5K4",
+  "prefix": "!",
+  "botName": "MangoVPNs bot",
+  "ownerId": "497249613158285313",
+  "sellyLink": "https://www.selly.gg/@MangoVPNss",
+  "sellyApiKey": "Crs2Mf_8grfsrvnzo5eqAAVWsrLpzcpqiqfN8M_xehiRcBDDzw", 
+  "sellyEmail": "cicatic@protonmail.com" 
+}
